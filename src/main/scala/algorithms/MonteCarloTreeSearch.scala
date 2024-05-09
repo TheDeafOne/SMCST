6e3d3f271ecf1716a4a64a1aeee028f9f65c5a38
TheDeafOne <Woodburnkb20@gcc.edu>
2024-05-09 17:25:43 -0400

    monte carlo run

--- src/main/scala/algorithms/MonteCarloTreeSearch.scala
+++ src/main/scala/algorithms/MonteCarloTreeSearch.scala
@@ -1,13 +1,21 @@
 package algorithms
 
-import games.*
+import games.{TicTacToeGameState, *}
 import games.Players.*
-import scala.util.Random
 
+import scala.util.Random
 import scala.annotation.tailrec
+import scala.collection.parallel.CollectionConverters.*
 
 val RANDOM_SEED = 42
 val rand = new Random(RANDOM_SEED)
+
+class MCTSMove(val board: TicTacToeBoard) {
+  val state = new TicTacToeGameState(Players.Player1, board)
+  val root = new Node(state, null, null)
+  val mcts = new MonteCarloTreeSearch(root, 2000, 5)
+  val move = mcts.search(root)
+}
 
 
 class MonteCarloTreeSearch(val root: Node, val maxIterations: Int = 10, val maxRolloutsPerIteration: Int = 10) {
@@ -42,6 +50,42 @@ class MonteCarloTreeSearch(val root: Node, val maxIterations: Int = 10, val maxR
 
 
     val node = root.children.maxBy(n => 1 - n.wins/n.visits)
+    (node, node.move)
+  }
+}
+
+class ParallelMonteCarloTreeSearch(val root: Node, val maxIterations: Int = 10, val maxRolloutsPerIteration: Int = 10) {
+  def search(root: Node = root): (Node, Move) = {
+    // selection
+    var iterations = 0
+    var current = root
+    while (iterations < maxIterations) {
+      if (current.children.nonEmpty) {
+        // exploration
+        current = current.children.maxBy(_.UCB1)
+      } else {
+        if (current.visits == 0) {
+          // rollout
+          (1 to maxRolloutsPerIteration).par.foreach(_ => current.backprop(current.rollout))
+          current = root
+        } else {
+          // expansion
+          if (!current.state.hasWinner) {
+            val moves = current.state.getMoves
+            moves.foreach(move => {
+              val newState = current.state.copy
+              newState.makeMove(move)
+              current.children = new Node(newState, current, move) :: current.children
+            })
+          }
+        }
+      }
+
+      iterations += 1
+    }
+
+
+    val node = root.children.maxBy(n => 1 - n.wins / n.visits)
     (node, node.move)
   }
 }
